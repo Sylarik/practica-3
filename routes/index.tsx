bd22e9fba3e78dcb155d51818d86eceeec6ba757
Sara García Galán <97830861+Sylarik@users.noreply.github.com>
2024-04-15 20:26:46 +0200

    Update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -34,7 +34,7 @@ export default function Home( props:PageProps) {
     <body class="index">
       <div class="contenedor">
         <div style="text-align: center;">
-          <img src="https://ov12-engine.flamingtext.com/netfu/tmp28015/coollogo_com-8171940.png" alt="tinder" style="width: 700px; height: 100%; displey: flex; align-items: center;"/>
+          <img src="https://files.catbox.moe/s5seq9.png" alt="tinder" style="width: 700px; height: 100%; displey: flex; align-items: center;"/>
         </div>
         <p> buscas una a alguien especial? Conoce a nuestros solteros con los que podras conectar y no solo mentalmente rawr</p>
         <div>
